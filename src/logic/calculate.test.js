a11293922ec69afb4e07f44a98e6b66a94855f43
yayner <yayner2002@gmail.com>
2022-08-10 16:17:44 +0300

    Add more test to calculate function

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,4 +1,5 @@
-import calculate from "./calculate";
+import calculate from './calculate';
+import operate from './operate';
 
 // arrange
 const mockState = {
@@ -7,26 +8,42 @@ const mockState = {
   operation: null,
 };
 
-describe("CALCULATE TESTS\n", () => {
-  test("should reset all state (AC)", () => {
-    expect(calculate(mockState, "AC")).toStrictEqual({
+describe('CALCULATE TESTS\n', () => {
+  test('should reset all state (AC)', () => {
+    expect(calculate(mockState, 'AC')).toStrictEqual({
       total: null,
       next: null,
       operation: null,
     });
   });
-  test("should update number", () => {
-    expect(calculate(mockState, "1")).toStrictEqual({
+  test('should update number', () => {
+    expect(calculate(mockState, '1')).toStrictEqual({
       total: null,
-      next: "1"
+      next: '1',
     });
   });
-  test("should add operation", () => {
-    const numState = {...mockState, next : '1'}
-    expect(calculate(numState, "+")).toStrictEqual({
-      operation: "+",
-      total: "1",
-      next: null
+  test('should add operation', () => {
+    const numState = { ...mockState, next: '1' };
+    expect(calculate(numState, '+')).toStrictEqual({
+      operation: '+',
+      total: '1',
+      next: null,
+    });
+  });
+});
+
+// add test to the calculate function
+describe('should return correct calculation', () => {
+  test('should return null if the button name is AC', () => {
+    const obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    expect(calculate(obj, 'AC')).toEqual({
+      total: null,
+      next: null,
+      operation: null,
     });
   });
 });
